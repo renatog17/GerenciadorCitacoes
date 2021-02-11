fcc0fce08704b273fda9730c449ff553e4cb7c7e
Renato Oliveira <renatorgo@outlook.com>
2021-02-10 21:50:32 -0300

    CRUD citacao

--- Ciacoes/src/main/java/com/renato/citacoes/resources/CitacaoResource.java
+++ Ciacoes/src/main/java/com/renato/citacoes/resources/CitacaoResource.java
@@ -1,11 +1,15 @@
 package com.renato.citacoes.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.renato.citacoes.domain.Citacao;
 import com.renato.citacoes.services.CitacaoService;
@@ -18,8 +22,30 @@ public class CitacaoResource {
 	CitacaoService citacaoService;
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
-	public ResponseEntity<?> find(@PathVariable Integer id){
+	public ResponseEntity<Citacao> find(@PathVariable Integer id){
 		Citacao citacao = citacaoService.findById(id);
 		return ResponseEntity.ok().body(citacao);
 	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Citacao obj){
+		obj = citacaoService.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody Citacao obj, @PathVariable Integer id){
+		obj.setId(id);
+		obj = citacaoService.update(obj);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Integer id){
+		citacaoService.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
 }
--- Ciacoes/src/main/java/com/renato/citacoes/services/CitacaoService.java
+++ Ciacoes/src/main/java/com/renato/citacoes/services/CitacaoService.java
@@ -3,10 +3,12 @@ package com.renato.citacoes.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.renato.citacoes.domain.Citacao;
 import com.renato.citacoes.repositories.CitacaoRepository;
+import com.renato.citacoes.services.exceptions.DataIntegrityException;
 import com.renato.citacoes.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -21,4 +23,22 @@ public class CitacaoService {
 				+ "Id: "+id+", Tipo: "+Citacao.class.getName()));
 	}
 	
+	public Citacao insert(Citacao obj) {
+		obj.setId(null);
+		return citacaoRepository.save(obj);
+	}
+	
+	public Citacao update(Citacao obj) {
+		findById(obj.getId());
+		return citacaoRepository.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		findById(id);
+		try {
+			citacaoRepository.deleteById(id);
+		}catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível deletar uma citação com comentários!");
+		}
+	}
 }
